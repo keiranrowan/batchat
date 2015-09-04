2b7d1c676242ab32fbef9079b91d6dc58fb5be77
Harold H. Kibb <hhkibb@airmail.cc>
2015-09-04 13:16:32 -0400

    Update in.bat

--- in.bat
+++ in.bat
@@ -5,5 +5,20 @@ cls
 echo ==================== Chat ========================
 :a
 set /p msg=~
+if "%msg%"==".exit" (
+	exit
+)
+if "%msg%"==".shell" (
+	cmd
+)
+if "%msg%"==".username" (
+	set /p username=New Username:
+	goto a
+)
+if "%msg%"==".file" (
+	set /p file=New Chat File Location:
+	goto a
+)
+
 echo [%username%] %msg% >> %file%
 goto a
